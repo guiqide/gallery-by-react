33bf3febc4659b7db1b48ed7195f62d1dfc33ee8
guiqd <qide.gui@midea.com>
2016-12-06 16:10:44 +0800

    gen stage

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,36 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+// 获取图片相关信息
+let imageDatas = require('../data/imageDatas.json');
+
+// 利用IIFE，通过图片名信息生成出图片URL路径信息
+imageDatas = (function genImageURL(imageDatasArr) {
+  for (var i = 0; i < imageDatasArr.length; i++) {
+    var singleImageData = imageDatasArr[i];
+
+    singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+
+    imageDatasArr[i] = singleImageData;
+  }
+
+  return imageDatasArr;
+})(imageDatas);
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <section className="stage">
+        <section className="img-sec">
+
+        </section>
+
+        <nav className="controller-nav">
+
+        </nav>
+      </section>
     );
   }
 }
@@ -19,4 +38,4 @@ class AppComponent extends React.Component {
 AppComponent.defaultProps = {
 };
 
-export default AppComponent;
+export default AppComponent
